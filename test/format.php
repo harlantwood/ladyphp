028f6dd244b8344b1eb33da44396d46c558e6838
unu <unumin@gmail.com>
2012-12-04 12:02:09 +0100

    updated readme

--- test/format.php
+++ test/format.php
@@ -6,7 +6,7 @@
 require_once __DIR__ . '/../lady.php';
 Lady::register();
 
-echo "## Example\n\n";
+echo "## Example\n\n```\n";
 $max = [0, 0];
 $sources[] = explode("\n", trim(file_get_contents(__DIR__ . '/example.lady')));
 $sources[] = explode("\n", trim(Lady::parseFile(__DIR__ . '/example.lady')));
@@ -14,7 +14,8 @@ foreach ($sources as $i => $source)
   foreach ($source as $n => $line)
     $max[$i] = max($max[$i], mb_strlen($line));
 foreach ($sources[0] as $n => $line)
-  echo rtrim(sprintf('    %-' . $max[0] . 's         | %-' . $max[1] . "s", $line, $sources[1][$n])) . "\n";
+  echo rtrim(sprintf('%-' . $max[0] . 's         | %-' . $max[1] . "s", $line, $sources[1][$n])) . "\n";
 
-echo "\n#### Output\n\n    ";
+echo "```\n\n#### Output\n\n```xml\n";
 require 'lady://' . __DIR__ . '/example.lady';
+echo "```";
